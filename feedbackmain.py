7e9c75562136546160cfb570f4d4dcd636058ca3
Tom Rathgeb <tom.rathgeb@studmail.htw-aalen.de>
2023-05-28 00:14:12 +0200

    cleaned up text by a minor amount

--- feedbackmain.py
+++ feedbackmain.py
@@ -16,10 +16,10 @@ def buttonandoutput():
     with col1:
         
         if st.session_state["File_uploaded"] is True:
-            st.header("Your text and True")
+            st.header("Your text")
 
         else:
-            st.header("Your text and False")
+            st.header("Your text")
             st.session_state[""] = " "
 
         st.text_area("literary masterpiece", st.session_state["story_string"], height=500, placeholder="Write your story here", key="text2") 
--- writingmain.py
+++ writingmain.py
@@ -3,10 +3,10 @@ import streamlit as st
 def writeandprompt():
 
     if st.session_state["File_uploaded"] is True:
-        st.header("Your text and True")
+        st.header("Your text")
 
     else:
-        st.header("Your text and False")
+        st.header("Your text")
         st.session_state["story_string"] = " "
 
     st.text_area("literary masterpiece",st.session_state["story_string"], height=500, placeholder="Write your story here", key="text")    
